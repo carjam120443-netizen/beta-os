7a8fa29b136efa8fd78cb1643f87f2c36d4ac9a7
carjam120443-netizen <carjam120443@gmail.com>
2026-09-06 13:04:46 -0400

    Extend userspace loader API

--- kernel/exec.h
+++ kernel/exec.h
@@ -1,20 +1,12 @@
 #ifndef BETA_EXEC_H
 #define BETA_EXEC_H
-
 #include <stddef.h>
 #include <stdint.h>
-
 #define EXEC_MAX_PROGRAMS 16
-
-typedef struct {
-    const char *name;
-    uintptr_t entry;
-    size_t size;
-} program_t;
-
+typedef struct { const char *name; uintptr_t entry; size_t size; } program_t;
 void exec_init(void);
 int exec_register(const char *name, uintptr_t entry, size_t size);
 const program_t *exec_find(const char *name);
 int exec_validate_elf64(const uint8_t *image, size_t size);
-
+int exec_load_elf64(const uint8_t *image, size_t size, uint8_t *dest, size_t dest_size, uintptr_t *entry);
 #endif
