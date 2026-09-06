793220041ccd75d052f96e30d30adaaedf6a30c9
carjam120443-netizen <carjam120443@gmail.com>
2026-09-06 13:00:18 -0400

    Add Beta OS userspace program loader interface

---
+++ kernel/exec.h
@@ -0,0 +1,20 @@
+#ifndef BETA_EXEC_H
+#define BETA_EXEC_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define EXEC_MAX_PROGRAMS 16
+
+typedef struct {
+    const char *name;
+    uintptr_t entry;
+    size_t size;
+} program_t;
+
+void exec_init(void);
+int exec_register(const char *name, uintptr_t entry, size_t size);
+const program_t *exec_find(const char *name);
+int exec_validate_elf64(const uint8_t *image, size_t size);
+
+#endif
